d2b93a679b25f1e4fa59057953f860ad6ca1009e
Daniel Pecos Martinez <contact@danielpecos.com>
2018-04-09 13:44:53 +0200

    Short format for command listing (closes #4)

--- cmd/list.go
+++ cmd/list.go
@@ -8,20 +8,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var viewSnippet bool
 var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "A brief description of your command",
+	Use:     "list",
+	Aliases: []string{"l"},
+	Short:   "List the content of your cmdbox",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmdboxDB := db.Load(dbPath)
 		defer cmdboxDB.Close()
 
 		cmds := db.List()
 		for _, cmd := range cmds {
-			fmt.Printf("%d - %s - %s\n\n%s\n\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Green(cmd.CreatedAt), cmd.Cmd)
+			fmt.Printf("%d - %s - %s\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Green(cmd.CreatedAt))
+			if viewSnippet {
+				fmt.Printf("\n%s\n\n", cmd.Cmd)
+			}
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.Flags().BoolVarP(&viewSnippet, "view", "v", false, "Show code snippet")
 }
