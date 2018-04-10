6a9162621afee586c5a1653027b7ebd18f00561a
Daniel Pecos Martinez <contact@danielpecos.com>
2018-04-10 17:04:29 +0200

    Support for mult & single line entries

--- cmd/add.go
+++ cmd/add.go
@@ -11,8 +11,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func readString(label string) string {
-	fmt.Print(label)
+func readStringMulti(label string) string {
+	fmt.Printf("%s (empty line to finish): ", label)
 
 	arr := make([]string, 0)
 	scanner := bufio.NewScanner(os.Stdin)
@@ -30,6 +30,14 @@ func readString(label string) string {
 	return strings.TrimSpace(val)
 }
 
+func readString(label string) string {
+	fmt.Printf("%s: ", label)
+
+	reader := bufio.NewReader(os.Stdin)
+	val, _ := reader.ReadString('\n')
+	return strings.TrimSpace(val)
+}
+
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new command to your cmdbox",
@@ -39,10 +47,10 @@ var addCmd = &cobra.Command{
 		defer cmdboxDB.Close()
 
 		command := models.Cmd{
-			Cmd:         readString("Command / snippet: "),
-			Title:       readString("Title: "),
-			Description: readString("Description: "),
-			URL:         readString("URL: "),
+			Title:       readString("Title"),
+			Description: readStringMulti("Description"),
+			URL:         readString("URL"),
+			Cmd:         readStringMulti("Command / snippet"),
 		}
 
 		db.Add(command)
