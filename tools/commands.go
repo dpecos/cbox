866e50b7ad55211e00e2ebc9f179c1a753ec3621
Daniel Pecos Martinez <contact@danielpecos.com>
2018-04-11 08:38:39 +0200

    Color output changed

--- tools/commands.go
+++ tools/commands.go
@@ -12,14 +12,14 @@ func PrintCommand(cmd models.Cmd, full bool, sourceOnly bool) {
 	if !sourceOnly {
 		if len(cmd.Tags) != 0 {
 			tags := strings.Join(cmd.Tags, ", ")
-			fmt.Printf("%d - (%s) %s - %s\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Brown(tags), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Green(cmd.CreatedAt))
+			fmt.Printf("%d - (%s) %s - %s\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Brown(tags), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Cyan(cmd.CreatedAt))
 		} else {
-			fmt.Printf("%d - %s - %s\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Green(cmd.CreatedAt))
+			fmt.Printf("%d - %s - %s\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Cyan(cmd.CreatedAt))
 
 		}
 		if full {
 			if cmd.Description != "" {
-				fmt.Printf("\n%s\n", aurora.Cyan(cmd.Description))
+				fmt.Printf("\n%s\n", aurora.Green(cmd.Description))
 			}
 			if cmd.URL != "" {
 				fmt.Printf("\n%s\n", aurora.Blue(cmd.URL))
