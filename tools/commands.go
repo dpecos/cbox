d0ed658dcd9550d3886b8a921836f923da64a0ab
Daniel Pecos Martinez <contact@danielpecos.com>
2018-04-12 20:21:14 +0200

    Print shorter timestamps (closes #15)

--- tools/commands.go
+++ tools/commands.go
@@ -12,11 +12,12 @@ func PrintCommand(cmd models.Cmd, full bool, sourceOnly bool) {
 	if !sourceOnly {
 		if len(cmd.Tags) != 0 {
 			tags := strings.Join(cmd.Tags, ", ")
-			fmt.Printf("%d - (%s) %s - %s\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Brown(tags), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Cyan(cmd.CreatedAt))
+			fmt.Printf("%d - (%s) %s", aurora.Red(aurora.Bold(cmd.ID)), aurora.Brown(tags), aurora.Blue(aurora.Bold(cmd.Title)))
 		} else {
-			fmt.Printf("%d - %s - %s\n", aurora.Red(aurora.Bold(cmd.ID)), aurora.Blue(aurora.Bold(cmd.Title)), aurora.Cyan(cmd.CreatedAt))
-
+			fmt.Printf("%d - %s", aurora.Red(aurora.Bold(cmd.ID)), aurora.Blue(aurora.Bold(cmd.Title)))
 		}
+		t := cmd.CreatedAt
+		fmt.Println(aurora.Sprintf(aurora.Cyan(" %d-%02d-%02d %02d:%02d:%02d"), t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second()))
 		if full {
 			if cmd.Description != "" {
 				fmt.Printf("\n%s\n", aurora.Green(cmd.Description))
