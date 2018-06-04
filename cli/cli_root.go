bf1e669b7c938eef8ac15a4ea7d8c47096e41af4
Daniel Pecos Martinez <contact@danielpecos.com>
2018-06-04 14:35:03 +0200

    Remove viper for settings support

--- cli/cli_root.go
+++ cli/cli_root.go
@@ -1,18 +1,9 @@
 package cli
 
 import (
-	"fmt"
 	"log"
-	"path/filepath"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-)
-
-var (
-	cfgFile string
-	dbPath  string
 )
 
 var rootCmd = &cobra.Command{
@@ -21,8 +12,6 @@ var rootCmd = &cobra.Command{
 	Long:  ``,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
@@ -31,43 +20,8 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cbox.yaml)")
-	rootCmd.PersistentFlags().StringVar(&dbPath, "path", "", "path where to store your cbox database (default is $HOME/.cbox.db)")
-
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Search config in home directory with name ".cbox" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cbox")
-	}
-
-	if dbPath == "" {
-		home, err := homedir.Dir()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		dbPath = filepath.Join(home, ".cbox.db")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	// TODO initialize home dir structure
 }
--- repository/repository_spaces.go
+++ repository/repository_spaces.go
@@ -7,7 +7,19 @@ import (
 	"os"
 
 	"github.com/dpecos/cbox/models"
+	homedir "github.com/mitchellh/go-homedir"
 )
+
+const DEFAULT_SPACE_NAME = "default"
+
+func resolveSpaceFile(spaceName string) string {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return home + "/.cbox/spaces/" + spaceName + ".json"
+}
 
 func SpaceStore(name string, title string) models.Space {
 	space := models.Space{
@@ -20,7 +32,7 @@ func SpaceStore(name string, title string) models.Space {
 		log.Fatalf("Error generating json for space %s: %s", name, err)
 	}
 
-	err = ioutil.WriteFile(name+".json", raw, os.ModeExclusive)
+	err = ioutil.WriteFile(resolveSpaceFile(name), raw, os.ModeExclusive)
 	if err != nil {
 		log.Fatalf("Error writing json file for space %s: %s", name, err)
 	}
@@ -29,18 +41,18 @@ func SpaceStore(name string, title string) models.Space {
 }
 
 func SpaceDelete(space models.Space) {
-	err := os.Remove(space.Name + ".json")
+	err := os.Remove(resolveSpaceFile(space.Name))
 	if err != nil {
 		log.Fatalf("Error deleting space %s: %s", space.Name, err)
 	}
 }
 
 func SpacesList() []string {
-	return []string{"default"}
+	return []string{DEFAULT_SPACE_NAME}
 }
 
 func SpaceLoad(name string) models.Space {
-	raw, err := ioutil.ReadFile(name + ".json")
+	raw, err := ioutil.ReadFile(resolveSpaceFile(name))
 	if err != nil {
 		log.Fatalf("Error loading json file for space %s: %s", name, err)
 	}
