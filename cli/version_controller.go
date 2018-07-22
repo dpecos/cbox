c7eb7539bbba0084e15f75ca83007fe38dbd5547
Daniel Pecos Martinez <contact@danielpecos.com>
2018-07-22 21:45:07 +0200

    cbox logo colorized

--- cli/version_controller.go
+++ cli/version_controller.go
@@ -11,7 +11,7 @@ var cboxVersion = "development"
 var cboxBuild = "-"
 
 func (ctrl *CLIController) Version(cmd *cobra.Command, args []string) {
-	fmt.Printf("%s", tools.Logo)
+	fmt.Printf("%s\n", tools.Logo)
 	fmt.Printf("Version: %s\n", cboxVersion)
 	fmt.Printf("Build: %s\n", cboxBuild)
 	fmt.Printf("Homepage: https://cbox.dplabs.io\n")
--- tools/cbox_meta.go
+++ tools/cbox_meta.go
@@ -1,13 +1,16 @@
 package tools
 
+import (
+	"github.com/dpecos/cbox/tools/console"
+)
+
 var (
 	// http://patorjk.com/software/taag/#p=testall&f=Flower%20Power&t=cbox
 	// Font Name: Ogre
-	Logo = `      _               
+	Logo = console.ColorBoldBlue(`      _               
   ___| |__   _____  __
  / __| '_ \ / _ \ \/ /
 | (__| |_) | (_) >  < 
- \___|_.__/ \___/_/\_\
-											
-`
+ \___|_.__/ \___/_/\_\							
+`)
 )
