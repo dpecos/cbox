b1d5ae354f36c172444defea75f296ef93810089
Daniel Pecos Martinez <contact@danielpecos.com>
2018-07-27 23:48:57 +0200

    Version output colorized

--- cli/version_controller.go
+++ cli/version_controller.go
@@ -3,17 +3,18 @@ package cli
 import (
 	"fmt"
 
+	"github.com/dpecos/cbox/tools/console"
+
+	"github.com/dpecos/cbox/core"
 	"github.com/dpecos/cbox/tools"
 	"github.com/spf13/cobra"
 )
 
-var cboxVersion = "development"
-var cboxBuild = "-"
-
 func (ctrl *CLIController) Version(cmd *cobra.Command, args []string) {
 	fmt.Printf("%s\n", tools.Logo)
-	fmt.Printf("Version: %s\n", cboxVersion)
-	fmt.Printf("Build: %s\n", cboxBuild)
-	fmt.Printf("Homepage: https://cbox.dplabs.io\n")
-	fmt.Printf("Author: Daniel Pecos Martinez\n")
+	fmt.Printf("%s %s\n", console.ColorBoldBlack("Version:"), core.Version)
+	fmt.Printf("%s %s\n", console.ColorBoldBlack("Build:"), core.Build)
+	fmt.Printf("%s %s\n", console.ColorBoldBlack("Homepage:"), "https://cbox.dplabs.io")
+	fmt.Printf("%s %s\n", console.ColorBoldBlack("Cloud:"), core.CloudURL())
+	fmt.Printf("%s %s\n", console.ColorBoldBlack("Author:"), "Daniel Pecos Martinez")
 }
