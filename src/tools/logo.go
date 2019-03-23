6e70dc2b2d34edb9e90b39fe3f593b7bfb6d6e01
Daniel Pecos Martinez <me@danielpecos.com>
2019-03-23 22:52:38 +0100

    New logo

--- src/tools/logo.go
+++ src/tools/logo.go
@@ -5,10 +5,11 @@ import "github.com/dplabs/cbox/src/tools/tty"
 var (
 	// http://patorjk.com/software/taag/#p=testall&f=Flower%20Power&t=cbox
 	// Font Name: Ogre
-	Logo = tty.ColorBoldBlue(`      _               
-  ___| |__   _____  __
- / __| '_ \ / _ \ \/ /
-| (__| |_) | (_) >  < 
- \___|_.__/ \___/_/\_\							
+	// > symbol from Graceful
+	Logo = tty.ColorBoldBlue(`           _               
+__     ___| |__   _____  __
+\ \   / __| '_ \ / _ \ \/ /
+ ) ) | (__| |_) | (_) >  < 
+/_/   \___|_.__/ \___/_/\_\							
 `)
 )
